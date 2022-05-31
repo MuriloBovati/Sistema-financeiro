e6f2925dd32acddc3bbb143b1f89e1f438642cd0
Murilo Bovati <murilo_bovati@hotmail.com>
2022-05-30 23:55:28 -0300

    att

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import Header from './Components/Header';
 import Infor from './Components/Infor';
+import axios from 'axios'
+import { useState } from 'react';
 
 function App() {
+  const [lista, setLista] = useState([])
+
+  const API = "localhost:5000"
+  axios.get(API).then()
+
   return (
     <div className="App">
       <Header/>
